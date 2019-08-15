c74686460d23600113b95fec5e98ad2a2abf7f9d
Mitchell Skaggs <skaggsm333@gmail.com>
2019-08-15 17:33:16 -0500

    Prevent macOS from trying to load librt

--- src/main/java/com/skaggsm/sharedmemory/posix/LibRT.java
+++ src/main/java/com/skaggsm/sharedmemory/posix/LibRT.java
@@ -2,13 +2,21 @@ package com.skaggsm.sharedmemory.posix;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
+import com.sun.jna.Platform;
 import com.sun.jna.Pointer;
 
 /**
  * Created by Mitchell Skaggs on 5/15/2019.
  */
 interface LibRT extends Library {
-    LibRT INSTANCE = Native.loadLibrary("rt", LibRT.class);
+    LibRT INSTANCE = Native.loadLibrary(getRTLibraryName(), LibRT.class);
+
+    static String getRTLibraryName() {
+        if (Platform.isMac())
+            return null;
+        else
+            return "rt";
+    }
 
     int shm_open(String name, int oFlag, int mode);
 
