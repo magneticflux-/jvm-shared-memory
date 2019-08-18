f0c407538209cf7b42552be5dc173e08811f6d79
Mitchell Skaggs <skaggsm333@gmail.com>
2019-08-17 20:46:35 -0500

    Add different headers for BSD/macOS

--- src/main/java/com/skaggsm/sharedmemory/posix/FCNTL.java
+++ src/main/java/com/skaggsm/sharedmemory/posix/FCNTL.java
@@ -1,13 +1,32 @@
 package com.skaggsm.sharedmemory.posix;
 
+import com.sun.jna.Platform;
+
 /**
  * Created by Mitchell Skaggs on 5/15/2019.
  */
 
-interface FCNTL {
-    int O_CREAT = 0x0200;
+@SuppressWarnings("OctalInteger")
+abstract class FCNTL {
+    final static int O_CREAT;
+    final static int O_RDWR;
 
-    int O_RDONLY = 0;
-    int O_WRONLY = 1;
-    int O_RDWR = 2;
+    static {
+        switch (Platform.getOSType()) {
+            case Platform.MAC:
+            case Platform.FREEBSD:
+            case Platform.OPENBSD:
+            case Platform.KFREEBSD:
+            case Platform.NETBSD:
+                O_CREAT = 0x0200;
+
+                O_RDWR = 2;
+                break;
+            default:
+                O_CREAT = 0100;
+
+                O_RDWR = 02;
+                break;
+        }
+    }
 }
--- src/main/java/com/skaggsm/sharedmemory/posix/STAT.java
+++ src/main/java/com/skaggsm/sharedmemory/posix/STAT.java
@@ -1,11 +1,34 @@
 package com.skaggsm.sharedmemory.posix;
 
+import com.sun.jna.Platform;
+
 /**
  * Created by Mitchell Skaggs on 5/16/2019.
  */
 
 @SuppressWarnings("OctalInteger")
-interface STAT {
-    int S_IRUSR = 00400;
-    int S_IWUSR = 00200;
+abstract class STAT {
+    final static int S_IRUSR;
+    final static int S_IWUSR;
+
+    static {
+        switch (Platform.getOSType()) {
+            case Platform.MAC:
+            case Platform.FREEBSD:
+            case Platform.OPENBSD:
+            case Platform.KFREEBSD:
+            case Platform.NETBSD:
+
+                S_IRUSR = 0000400;
+                S_IWUSR = 0000200;
+
+                break;
+            default:
+
+                S_IRUSR = 0400;
+                S_IWUSR = 0200;
+
+                break;
+        }
+    }
 }
